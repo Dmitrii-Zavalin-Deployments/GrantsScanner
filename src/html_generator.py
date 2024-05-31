3b56c3e4d0aae5eec53ad485e523c1748346715a
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2024-05-31 16:10:12 -0300

    First Stable Version of html generator

--- src/html_generator.py
+++ src/html_generator.py
@@ -85,19 +85,19 @@ class HTMLGenerator:
             </div>
 
             <script>
-                function openTab(evt, tabName) {
+                function openTab(evt, tabName) {{
                     var i, tabcontent, tablinks;
                     tabcontent = document.getElementsByClassName("tabcontent");
-                    for (i = 0; i < tabcontent.length; i++) {
+                    for (i = 0; i < tabcontent.length; i++) {{
                         tabcontent[i].className = tabcontent[i].className.replace(" active-tab", "");
-                    }
+                    }}
                     tablinks = document.getElementsByClassName("tablinks");
-                    for (i = 0; i < tablinks.length; i++) {
+                    for (i = 0; i < tablinks.length; i++) {{
                         tablinks[i].className = tablinks[i].className.replace(" active", "");
-                    }
+                    }}
                     document.getElementById(tabName).className += " active-tab";
                     evt.currentTarget.className += " active";
-                }
+                }}
             </script>
         </body>
         </html>
