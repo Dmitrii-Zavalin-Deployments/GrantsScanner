12d0488ae333437bbc18bcb742523d4eef3d56d1
Dmitrii Zavalin <zavalin.dmitry@gmail.com>
2024-05-31 10:44:40 -0300

    Added Query Builder to main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from src.pdf_parser import PDFParser
 
 def main():
     # Initialize modules
-    # query_builder = QueryBuilder()
+    query_builder = QueryBuilder()
     # search_executor = SearchExecutor()
     html_generator = HTMLGenerator('data/grants.json')
     downloader = PDFDownloader()
@@ -16,7 +16,9 @@ def main():
     # ... other module initializations
 
     # Build the search query
-    # query = query_builder.build_query()
+    query_data = query_builder.get_query_data()
+    print(f'Domain: {query_data["name"]}')
+    print(f'Query: {query_data["query"]}')
     
     # Execute the search and get PDF links
     pdf_links = ["https://www.eggfarmers.ca/wp-content/uploads/2024/01/2024-Call-for-LOIs_Applicant-Information-Package_ENG.pdf"] # search_executor.execute_search(query)
